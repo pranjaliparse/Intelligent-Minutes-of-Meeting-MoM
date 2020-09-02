42856b9466d53643f5e30d8e16fce589f6e51cc4
Pranjali Ajay Parse <pranjaliparse@gmail.com>
2020-09-02 15:10:41 +0530

    Optimized the readability and added comments

--- keyword_extraction/main.py
+++ keyword_extraction/main.py
@@ -1,14 +1,18 @@
 import datetime
-from google.cloud import storage
 import json
 from monkeylearn import MonkeyLearn
-
+from google.cloud import storage
 
 def read_data_from_storage(bucket_name, file_name):
+    """Reads a file from Cloud Storage."""
+    # bucket_name = "your-bucket-name"
+    # file_name = "path-to-the-file-name"
+
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(file_name)
     blob = blob.download_as_string()
+    print("Read data from {}.".format(file_name))
     return blob
 
 
@@ -22,7 +26,6 @@ def upload_blob(source_bucket_name, source_file_name, destination_blob_name):
     bucket = storage_client.bucket(source_bucket_name)
     blob = bucket.blob(destination_blob_name)
     blob.upload_from_filename(source_file_name)
-
     print(
         "File {} uploaded to {}.".format(
             source_file_name, destination_blob_name
@@ -31,31 +34,31 @@ def upload_blob(source_bucket_name, source_file_name, destination_blob_name):
 
 
 def extract_keywords(input_text):
-    ml = MonkeyLearn('f32fd99c82f87fd954d937cd179d1c665fdd57fe')
-    model_id = 'ex_YCya9nrn'
+    """Extract Keywords from input text using MonkeyLearn API."""
+    # input_text = "your-input-text"
 
     keywords = []
-    data = [input_text]
-    result = ml.extractors.extract(model_id, data)
-
-    for keyword in result.body[0]['extractions']:
-        keywords.append(keyword['parsed_value'])
-
-    print("Keywords:",keywords)
+    ml = MonkeyLearn('f32fd99c82f87fd954d937cd179d1c665fdd57fe')
+    result = ml.extractors.extract('ex_YCya9nrn', [input_text])
+    for keyword in result.body[0]['extractions']: keywords.append(keyword['parsed_value'])
     return keywords
 
+
 def common_keywords(transcribed_text, final_text):
-    math_words = [x for x in extract_keywords('\n'.join(x for x in (transcribed_text).split(" ")))]
-    for x in list(extract_keywords('\n'.join(x for x in (final_text).split(" ")))):
-        math_words.append(x)
+    """Extract Common Keywords from ocr and transcribed text from input video"""
+    # transcribed_text = "your-transcription-from-video"
+    # final_text = "your-ocr-text-from-video"
 
     keywords = []
-    for i in math_words:
-        if i in transcribed_text:
-            keywords.append(i)
-
+    math_words = [x for x in extract_keywords('\n'.join(x for x in (transcribed_text).split(" ")))]
+    for x in list(extract_keywords('\n'.join(x for x in (final_text).split(" ")))): math_words.append(x)
+    for word in math_words: 
+        if word in str.lower(transcribed_text): keywords.append(word)
     keywords = list(set(keywords))
-    return(keywords)
+    print("Extracted Keywords from Input Text..")
+    print("Keywords: {}".format(keywords))
+    return keywords
+
 
 def transcribe_video_pipeline(event, context):
     """Background Cloud Function to be triggered by Cloud Storage.
@@ -71,6 +74,7 @@ def transcribe_video_pipeline(event, context):
         None; the output is written to Stackdriver Logging
     """
 
+    print("Started Keyword Extraction..")
     print('Event ID: {}'.format(context.event_id))
     print('Event type: {}'.format(context.event_type))
     print('Bucket: {}'.format(event['bucket']))
@@ -80,8 +84,8 @@ def transcribe_video_pipeline(event, context):
     print('Updated: {}'.format(event['updated']))
 
     file_path = "gs://{bucket_name}/{file_name}".format(bucket_name = event['bucket'], file_name = event['name'])
-    print(file_path)
-
+    print("File Path: {}.".format(file_path))
+    
     write_data = json.loads(read_data_from_storage(event['bucket'], event['name']))
     write_data['keywords'] = common_keywords(write_data['transcribed_text'], write_data['ocr_text'])
     print("Finished Keyword Extraction..")
